0231d2d75c762f34c066bf7eb4a6dd556fc7454c
root <root@itdili.aliyun>
2020-06-22 15:39:07 +0800

    docker-compose（经过测试

--- Minio/main.sh
+++ Minio/main.sh
@@ -1,2 +1,2 @@
-mc config host add minio1 http://116.62.166.181:9000 minioadmin minioadmin
-mc cp /AML/* minio1/aml
+mc config host add minio_server http://116.62.166.181:9000/ minioadmin minioadmin --api s3v4
+mc cp /AML/* minio_server/aml
